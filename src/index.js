664d277021cf88965597d7bc114119e008d725f9
Cameron Ray <cameron@cameronray.net>
2018-08-06 15:47:19 -0400

    Add regraph-request

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+const HOST = process.env.GRAPHQL_HOST || 'http://localhost:8001/graphql';
 
-import styles from './styles.css'
+export const Query = (WrappedComponent, query, variables = () => {}, host = HOST) => {
+  class WithRequest extends React.Component {
+    constructor(props) {
+      super(props);
+      this.getData = this.getData.bind(this);
+      this.state = {
+        data: {},
+      };
+      this.getData(variables(props));
+    }
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+    componentDidUpdate(prevProps) {
+      if (JSON.stringify(this.props) !== JSON.stringify(prevProps))
+        this.getData(variables(this.props));
+    }
+
+    getData(variables) {
+      let vars = Object.assign({}, this.state, variables);
+      vars.data = undefined;
+      return adHocRequest(host, query, vars)
+        .then(res => {
+          vars.data = res.data;
+          this.setState(vars);
+        })
+        .catch(err => {
+          vars.data = { err };
+          this.setState(vars);
+        });
+    }
+
+    render() {
+      return React.createElement(
+        WrappedComponent,
+        Object.assign({}, this.props, {
+          data: this.state.data ? this.state.data : {},
+          getData: this.getData,
+        })
+      );
+    }
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  return WithRequest;
+};
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+export const adHocRequest = (host, query, variables) => {
+  return fetch(host, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query, variables }),
+  }).then(res => res.json());
+};
